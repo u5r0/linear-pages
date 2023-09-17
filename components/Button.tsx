8b05d19e4ed8445d12c98b281edc160d972fb8c8
Aasim Attia <u5r0@pm.me>
2023-09-17 08:39:10 +0300

    Fix: button bg

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,20 +2,17 @@ import Link from 'next/link'
 import type { ComponentProps } from 'react';
 import { cva, type VariantProps } from 'class-variance-authority'
 
-import { cn } from '@/lib/utils'
-
 type ButtonProps = 
   VariantProps<typeof buttonVariants> 
   & ComponentProps<"a">
-  & {
-    children: React.ReactNode;
-  };
+  & { children: React.ReactNode; };
+
 
 const buttonVariants = cva("inline-flex items-center rounded-full font-medium", {
   variants: {
     variant: {
       primary: "bg-brand whitespace-nowrap hover:brightness-115 text-shadow-sm hover:text-shadow hover:shadow-high",
-      secondary: "bg-btn-200",
+      secondary: "bg-white bg-opacity-10 hover:bg-opacity-20 border border-gray-100",
     },
     size: {
       small: "text-xs px-3 h-7",
